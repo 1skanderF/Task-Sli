0db5a89f7cc276be8f8d67ab573e22a4b4f60dd4
Iskander Fajzullin <fajzullin2004@mail.ru>
2026-01-28 17:31:07 +0500

    Update storage

--- cmd/task-sli/main.go
+++ cmd/task-sli/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/1skander/Task-Ski/models"
+	"github.com/1skander/Task-Ski/storage"
 )
 
 func main() {
+	storage, err := storage.NewJSONStorage("tasks.json")
+	if err != nil {
+		fmt.Println("Ошибка создания хранилища:", err)
+		return
+	}
 
-	task := models.NewTask("Доделать задачу")
-	fmt.Println(task)
+	// Проверяем Load
+	err = storage.Load()
+	if err != nil {
+		fmt.Println("Ошибка загрузки:", err)
+	} else {
+		fmt.Println("Загрузка успешна")
+	}
 }
--- storage/storage.go
+++ storage/storage.go
@@ -1,14 +1,41 @@
 package storage
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 func (s *JSONStorage) Load() error {
-	file, err = os.Open(s.filename)
+
+	file, err1 := os.Open(s.filename)
+
+	// Проверяем файл найден?
+	if err1 != nil {
+		if os.IsNotExist(err1) {
+			// Если файла нет это не ошибка
+			return nil
+		}
+		return err1
+	}
+
 	defer file.Close()
 
-	if err != nil(
-		if os.IsNotExist(err)(
-			return nil
-		)
-	)
+	fileInfo, err2 := file.Stat()
+
+	if err2 != nil {
+		return err2
+	}
+	if fileInfo.Size() == 0 {
+		// Если файл пустой это нормально
+		return nil
+	}
+
+	decoder := json.NewDecoder(file)
+	err3 := decoder.Decode(&s.tasks)
+	if err3 != nil {
+		return fmt.Errorf("ошибка чтения JSON: %w", err3)
+	}
+
+	return nil
 }
