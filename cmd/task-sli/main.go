295eae950e9297626279f67283f0d97b150acaa8
Iskander Fajzullin <fajzullin2004@mail.ru>
2026-01-28 18:37:28 +0500

    Add Save()

--- cmd/task-sli/main.go
+++ cmd/task-sli/main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/1skander/Task-Ski/models"
 	"github.com/1skander/Task-Ski/storage"
 )
 
 func main() {
-	storage, err := storage.NewJSONStorage("tasks.json")
-	if err != nil {
-		fmt.Println("Ошибка создания хранилища:", err)
-		return
-	}
+	storage := storage.NewJSONStorage("tasks.json")
 
-	// Проверяем Load
-	err = storage.Load()
+	// Добавляем тестовую задачу
+	task := models.NewTask("Тест Save()")
+	task.ID = 1
+	storage.tasks[1] = task // пока напрямую, потом через AddTask
+
+	// Сохраняем
+	err := storage.Save()
 	if err != nil {
-		fmt.Println("Ошибка загрузки:", err)
+		fmt.Println("Ошибка Save():", err)
 	} else {
-		fmt.Println("Загрузка успешна")
+		fmt.Println("Save() успешен!")
+
+		// Читаем файл чтобы проверить
+		data, _ := os.ReadFile("tasks.json")
+		fmt.Println("Содержимое файла:")
+		fmt.Println(string(data))
 	}
 }
--- storage/storage.go
+++ storage/storage.go
@@ -8,23 +8,23 @@ import (
 
 func (s *JSONStorage) Load() error {
 
-	file, err1 := os.Open(s.filename)
+	file, err := os.Open(s.filename)
 
 	// Проверяем файл найден?
-	if err1 != nil {
-		if os.IsNotExist(err1) {
+	if err != nil {
+		if os.IsNotExist(err) {
 			// Если файла нет это не ошибка
 			return nil
 		}
-		return err1
+		return err
 	}
 
 	defer file.Close()
 
-	fileInfo, err2 := file.Stat()
+	fileInfo, err := file.Stat()
 
-	if err2 != nil {
-		return err2
+	if err != nil {
+		return err
 	}
 	if fileInfo.Size() == 0 {
 		// Если файл пустой это нормально
@@ -32,9 +32,45 @@ func (s *JSONStorage) Load() error {
 	}
 
 	decoder := json.NewDecoder(file)
-	err3 := decoder.Decode(&s.tasks)
-	if err3 != nil {
-		return fmt.Errorf("ошибка чтения JSON: %w", err3)
+	err = decoder.Decode(&s.tasks)
+	if err != nil {
+		return fmt.Errorf("ошибка чтения JSON: %w", err)
+	}
+
+	return nil
+}
+
+func (s *JSONStorage) Save() error {
+	// Блокируем параллельный вызов Save()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// Задаем имя для временного файла
+	tempFilename := s.filename + ".tmp"
+
+	// Создаем временный файл, если ошибка возращаем ошибку удаляем временный файл
+	tempFile, err := os.Create(tempFilename)
+	if err != nil {
+		return err
+	}
+
+	// Откладываем функцию закрытия
+	defer tempFile.Close()
+
+	// Создаем JSON-энкодер для файла, настраиваем отступы, проверяем ошибку
+	encoder := json.NewEncoder(tempFile)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(s.tasks)
+	if err != nil {
+		os.Remove(tempFilename)
+		return fmt.Errorf("ошибка записи JSON: %w", err)
+	}
+
+	// Перезаписываем файл
+	err = os.Rename(tempFilename, s.filename)
+	if err != nil {
+		os.Remove(tempFilename)
+		return err
 	}
 
 	return nil
